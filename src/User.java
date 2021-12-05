3b4c4b75e7ec85e85ef39bdf6ae1b86658632164
EliasSkooter <89127927+Elias-Haddad735@users.noreply.github.com>
2021-12-05 15:26:22 +0200

    making user UI

---
+++ src/User.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+
+public class User {
+    private JPanel panel1;
+    private JLabel pickATrustedUserLabel;
+    private JLabel numOfActiveCasesLabel;
+    private JPanel RegisterPanel;
+    private JLabel addUserLabel;
+    private JLabel statusLabel;
+    private JLabel uploadPRCLabel;
+    private JLabel locationLabel;
+    private JLabel ImageSelectedLabel;
+    private JLabel FileSelectedLabel;
+    private JButton signUpButton;
+    private JButton cancelButton;
+    private JButton QueryActive;
+    private JLabel valueActive;
+    private JComboBox listUsersTrusted;
+    private JComboBox ListUsersToTrust;
+    private JButton submitToTrust;
+    private JComboBox covidStatus;
+    private JButton SubmitStatus;
+    private JButton pcrUploadButton;
+    private JLabel pcrUploadLabel;
+    private JComboBox dropdownLocations;
+    private JButton submitLocation;
+    private String image;
+    private String vacCer;
+}
